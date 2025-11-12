cab7e4f5b3b9e5553faf4b3fc639282ebcc0ec41
Daniel Ramos <danielramospy001@gmail.com>
2025-11-12 12:42:17 -0300

    Remove duplicated logic for authentication

--- back/src/routes/task.routes.ts
+++ back/src/routes/task.routes.ts
@@ -2,14 +2,9 @@ import { Router } from 'express';
 import { TaskController } from '../controllers/TaskController';
 import { validateRequest } from '../middleware/validation.middleware';
 import { createTaskSchema, updateTaskSchema } from '../validators/task.validator';
-import { checkJwt } from '../middleware/auth.middleware';
-import { ensureUserExists } from '../middleware/user.middleware';
 
 const router = Router();
 const taskController = new TaskController();
-
-router.use(checkJwt);
-router.use(ensureUserExists);
 
 router.get('/', taskController.getAllTasks);
 router.get('/:id', taskController.getTaskById);
